63a53973dd62cd695baa90387b1de67fca5f419d
Jingkai <tanjingkai@me.com>
2017-12-11 12:14:35 +0800

    Restructured code
    Fixes #5 Fixes #4 restructured some code, wrote new function to avoid dupes

--- createdb.py
+++ createdb.py
@@ -9,46 +9,53 @@ import pymysql,traceback
 from contextlib import closing
 from tokens import Tokens
 
-class CreateDb():
 
+create_user_table_string = """
+							CREATE TABLE IF NOT EXISTS Cryptokitties.User (
+								telegram_id INT(15) NOT NULL,
+								generation_index INT(5) NULL,
+								cooldown_index INT(5) NULL,
+								offset_start INT(5) NULL,
+								offset_end INT(5) NULL,
+								alert VARCHAR(50) NULL,
+								PRIMARY KEY (telegram_id)
+								)
+							"""
+create_attributes_table_string = """
+									CREATE TABLE IF NOT EXISTS Cryptokitties.Attribute (
+										telegram_id INT(15),
+										attribute_name VARCHAR(50)
+										)
+								"""
+class CreateDb():
 	def create_user_table(self):
 		try:
 			with closing (pymysql.connect(Tokens.mysql('host'),Tokens.mysql('usn'),Tokens.mysql('pwd'),Tokens.mysql('db'),charset='utf8')) as conn:
 				conn.autocommit(True)
 				with closing(conn.cursor()) as cur:
-					cur.execute("""
-								CREATE TABLE IF NOT EXISTS Cryptokitties.User (
-									telegram_id INT(15) NOT NULL,
-									generation_index INT(5) NULL,
-									cooldown_index INT(5) NULL,
-									offset_start INT(5) NULL,
-									offset_end INT(5) NULL,
-									alert VARCHAR(50) NULL,
-									PRIMARY KEY (telegram_id)
-									)
-								""") #Change cryptokitties to whatever you call your db in tokens.
+					cur.execute(create_user_table_string) #Change cryptokitties to whatever you call your db in tokens.
 		except Exception as e:
 			catcherror = traceback.format_exc()
-			print(catcherror)
+			self.write_error(catcherror)
 		else:
-			print('User Table sucessfully created')
+			self.write_error('User Table Sucessfully created')
 
 	def create_attributes_table(self):
 		try:
 			with closing (pymysql.connect(Tokens.mysql('host'),Tokens.mysql('usn'),Tokens.mysql('pwd'),Tokens.mysql('db'),charset='utf8')) as conn:
 				conn.autocommit(True)
 				with closing(conn.cursor()) as cur:
-					cur.execute("""
-								CREATE TABLE IF NOT EXISTS Cryptokitties.Attribute (
-									telegram_id INT(15),
-									attribute_name VARCHAR(50)
-									)
-								""")
+					cur.execute(create_attributes_table_string)
 		except Exception as e:
 			catcherror = traceback.format_exc()
-			print(catcherror)
+			self.write_error(catcherror)
 		else:
-			print('Attributes Table sucessfully created')
+			self.write_error('Attributes table successfully created')
+
+	def write_error(self,error):
+		with open('logs.txt','a') as f:
+			f.write(str(error))
+			f.write(traceback.format_exc())
 
 if __name__ == '__main__':
 	CreateDb().create_user_table()
